6642c9baf95849d4af991da770b5c33661d0578e
Waqar Malik <wmalloc@users.noreply.github.com>
2021-04-09 12:50:44 -0700

    Fixed the zendesk and patient upload issue

--- Allie/Shared/CareKit/Model/CarePlanResponse.swift
+++ Allie/Shared/CareKit/Model/CarePlanResponse.swift
@@ -9,13 +9,13 @@ import CareKitStore
 import Foundation
 
 public struct CarePlanResponse: Codable {
-	public var carePlans: [CarePlan]
+	public var carePlans: [CarePlan]?
 	public var patients: [AlliePatient]?
-	public var tasks: [Task]
+	public var tasks: [Task]?
 	public var outcomes: [Outcome]?
 	public var vectorClock: [String: Int]
 
-	public init(carePlans: [CarePlan] = [], patients: [AlliePatient]? = nil, tasks: [Task] = [], outcomes: [Outcome]? = nil, vectorClock: [String: Int] = [:]) {
+	public init(carePlans: [CarePlan]? = nil, patients: [AlliePatient]? = nil, tasks: [Task]? = nil, outcomes: [Outcome]? = nil, vectorClock: [String: Int] = [:]) {
 		self.carePlans = carePlans
 		self.tasks = tasks
 		self.vectorClock = vectorClock
