e9bf6b111b85b3d35d87439c6574830539099c56
hypermobileshop99 <hypermobileshop99@gmail.com>
2022-03-24 04:37:07 -0700

    [v2/ui-refactor]: hero card

--- Allie/DailyTasks/NewDailyTasks/CustomView/DailyTaskTopView.swift
+++ Allie/DailyTasks/NewDailyTasks/CustomView/DailyTaskTopView.swift
@@ -26,7 +26,9 @@ class DailyTaskTopView: UIView {
 		let wholeStackView = UIStackView()
 		wholeStackView.translatesAutoresizingMaskIntoConstraints = false
 		wholeStackView.axis = .vertical
-		wholeStackView.alignment = .top
+		wholeStackView.alignment = .center
+		wholeStackView.distribution = .fill
+		wholeStackView.spacing = 0
 		return wholeStackView
 	}()
 
@@ -34,50 +36,49 @@ class DailyTaskTopView: UIView {
 		let greetingView = UIView()
 		greetingView.translatesAutoresizingMaskIntoConstraints = false
 		greetingView.backgroundColor = .white
+		greetingView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+		greetingView.layer.cornerRadius = 10.0
+		greetingView.setShadow()
 		return greetingView
+	}()
+
+	private var greetingStackView: UIStackView = {
+		let greetingStackView = UIStackView()
+		greetingStackView.translatesAutoresizingMaskIntoConstraints = false
+		greetingStackView.axis = .vertical
+		greetingStackView.alignment = .center
+		greetingStackView.distribution = .fill
+		greetingStackView.spacing = 20.0
+		return greetingStackView
+	}()
+
+	private var greetingImage: UIImageView = {
+		let greetingImage = UIImageView()
+		greetingImage.translatesAutoresizingMaskIntoConstraints = false
+		greetingImage.contentMode = .scaleAspectFit
+		return greetingImage
 	}()
 
 	private var greetingLabel: UILabel = {
 		let greetingLabel = UILabel()
 		greetingLabel.translatesAutoresizingMaskIntoConstraints = false
+		greetingLabel.numberOfLines = 0
+		greetingLabel.textAlignment = .center
 		return greetingLabel
 	}()
 
 	private let helloLabel: UILabel = {
 		let helloLabel = UILabel()
 		helloLabel.translatesAutoresizingMaskIntoConstraints = false
-		helloLabel.attributedText = "How are you\nfeeling today?".attributedString(style: .silkabold24, foregroundColor: .mainBlue)
 		helloLabel.numberOfLines = 0
+		helloLabel.textAlignment = .center
 		return helloLabel
-	}()
-
-	private var greetingStackView: UIStackView = {
-		let greetingStackView = UIStackView()
-		greetingStackView.translatesAutoresizingMaskIntoConstraints = false
-		greetingStackView.axis = .horizontal
-		greetingStackView.distribution = .fillEqually
-		greetingStackView.alignment = .fill
-		return greetingStackView
-	}()
-
-	private var statusView: UIView = {
-		let statusView = UIView()
-		statusView.translatesAutoresizingMaskIntoConstraints = false
-		statusView.backgroundColor = .mainGreen
-		return statusView
-	}()
-
-	private var statusLabel: UILabel = {
-		let statusLabel = UILabel()
-		statusLabel.translatesAutoresizingMaskIntoConstraints = false
-		statusLabel.font = .systemFont(ofSize: 20, weight: .bold)
-		statusLabel.textColor = .mainDarkGreen
-		return statusLabel
 	}()
 
 	private var calendarView: UIView = {
 		let calendarView = UIView()
 		calendarView.backgroundColor = .white
+		calendarView.setShadow()
 		calendarView.translatesAutoresizingMaskIntoConstraints = false
 		return calendarView
 	}()
@@ -123,52 +124,30 @@ class DailyTaskTopView: UIView {
 
 	private func setupViews() {
 		backgroundColor = .white
-		setShadow(shadowRadius: 10.0, opacity: 0.1)
 		wholeStackView.addArrangedSubview(greetingView)
-		greetingView.addSubview(greetingLabel)
-		greetingView.addSubview(helloLabel)
+		NSLayoutConstraint.activate([greetingView.leadingAnchor.constraint(equalTo: wholeStackView.leadingAnchor, constant: 10),
+		                             wholeStackView.trailingAnchor.constraint(equalTo: greetingView.trailingAnchor, constant: 10)])
 		greetingView.addSubview(greetingStackView)
+		NSLayoutConstraint.activate([greetingStackView.centerXAnchor.constraint(equalTo: greetingView.centerXAnchor),
+		                             greetingStackView.leadingAnchor.constraint(equalTo: greetingView.leadingAnchor),
+		                             greetingStackView.topAnchor.constraint(equalTo: greetingView.topAnchor, constant: 30),
+		                             greetingStackView.bottomAnchor.constraint(equalTo: greetingView.bottomAnchor, constant: -30)])
 
-		greetingLabel.topAnchor.constraint(equalTo: greetingView.topAnchor, constant: 48).isActive = true
-		greetingLabel.centerXAnchor.constraint(equalTo: greetingView.centerXAnchor).isActive = true
-		greetingLabel.leadingAnchor.constraint(equalTo: greetingView.leadingAnchor, constant: 32).isActive = true
+		[greetingImage, greetingLabel, helloLabel].forEach { greetingStackView.addArrangedSubview($0) }
+
+		NSLayoutConstraint.activate([greetingImage.widthAnchor.constraint(equalToConstant: 100),
+		                             greetingImage.heightAnchor.constraint(equalToConstant: 100)])
 
 		var greetingPatient = ""
 		if let patientName = careManager.patient?.displayName {
 			greetingPatient = "\(patientName)!"
 		}
-		greetingLabel.attributedText = "\(getDateState()) \(greetingPatient)".attributedString(style: .silkabold20, foregroundColor: .black)
 
-		helloLabel.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 12.0).isActive = true
-		helloLabel.leadingAnchor.constraint(equalTo: greetingView.leadingAnchor, constant: 32).isActive = true
-		helloLabel.centerXAnchor.constraint(equalTo: greetingView.centerXAnchor).isActive = true
+		let dateState = getDateState()
 
-		greetingView.widthAnchor.constraint(equalTo: wholeStackView.widthAnchor).isActive = true
-
-		greetingStackView.centerXAnchor.constraint(equalTo: greetingView.centerXAnchor).isActive = true
-		greetingStackView.leadingAnchor.constraint(equalTo: greetingView.leadingAnchor, constant: 32.0).isActive = true
-		greetingStackView.topAnchor.constraint(equalTo: helloLabel.bottomAnchor, constant: 24.0).isActive = true
-		greetingStackView.bottomAnchor.constraint(equalTo: greetingView.bottomAnchor, constant: -24).isActive = true
-		greetingStackView.addArrangedSubview(FeelingButton(image: UIImage(named: "button-mood-notgreat")!, selectedImage: UIImage(named: "button-mood-notgreat-selected")!, title: "Not Great", callback: { [self] in
-			statusLabel.text = "Not Great"
-			showAnimation(index: 0)
-		}))
-		greetingStackView.addArrangedSubview(FeelingButton(image: UIImage(named: "button-mood-okay")!, selectedImage: UIImage(named: "button-mood-okay-selected")!, title: "Ok", callback: { [self] in
-			statusLabel.text = "Ok"
-			showAnimation(index: 1)
-		}))
-		greetingStackView.addArrangedSubview(FeelingButton(image: UIImage(named: "button-mood-great")!, selectedImage: UIImage(named: "button-mood-great-selected")!, title: "Great!", callback: { [self] in
-			statusLabel.text = "Great!"
-			showAnimation(index: 2)
-		}))
-
-		wholeStackView.addArrangedSubview(statusView)
-		statusView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-		statusView.widthAnchor.constraint(equalTo: wholeStackView.widthAnchor).isActive = true
-
-		statusView.addSubview(statusLabel)
-		statusLabel.leadingAnchor.constraint(equalTo: statusView.leadingAnchor, constant: 32).isActive = true
-		statusLabel.bottomAnchor.constraint(equalTo: statusView.bottomAnchor, constant: -16).isActive = true
+		greetingLabel.attributedText = "\(dateState.greetings) \(greetingPatient)".attributedString(style: .silkabold20, foregroundColor: .mainGray)
+		helloLabel.attributedText = dateState.description.attributedString(style: .silkaregular17, foregroundColor: .allieLightGray)
+		greetingImage.image = UIImage(named: dateState.iconName)
 
 		wholeStackView.addArrangedSubview(calendarView)
 		calendarView.widthAnchor.constraint(equalTo: wholeStackView.widthAnchor).isActive = true
@@ -195,52 +174,38 @@ class DailyTaskTopView: UIView {
 		wholeStackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
 		wholeStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
 
-		statusView.isHidden = true
 		calendarView.isHidden = true
 
 		setButtonTitle(date: selectedDate)
+
+		showAnimation()
 	}
 
-	func showAnimation(index: Int) {
-		UIView.animate(withDuration: 0.3) { [self] in
-			greetingView.isHidden = true
-			self.layoutIfNeeded()
-		} completion: { [self] _ in
-			DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-				UIView.animate(withDuration: 0.3) {
-					statusView.isHidden = false
+	func showAnimation() {
+		DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+			UIView.animate(withDuration: 0.5) {
+				self.greetingView.isHidden = true
+				self.layoutIfNeeded()
+			} completion: { _ in
+				UIView.animate(withDuration: 0.5) {
+					self.calendarView.isHidden = false
 					self.layoutIfNeeded()
-				} completion: { _ in
-					DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-						UIView.animate(withDuration: 0.3) { [self] in
-							statusView.isHidden = true
-							self.layoutIfNeeded()
-						} completion: { [self] _ in
-							calendarView.isHidden = false
-							self.layoutIfNeeded()
-							if index == 0 {
-								DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
-									self?.delegate?.onClickNotGreat()
-								}
-							}
-						}
-					}
 				}
 			}
 		}
 	}
 
-	private func getDateState() -> String {
+	private func getDateState() -> DateStateModel {
 		let hour = Calendar.current.component(.hour, from: Date())
 		switch hour {
 		case 0 ..< 12:
-			return DateState.morning.greetings
+			return DateState.morning.dateState
 		case 12 ..< 16:
-			return DateState.afternoon.greetings
+			return DateState.afternoon.dateState
 		case 16 ..< 24:
-			return DateState.evening.greetings
+			return DateState.evening.dateState
 		default:
-			return DateState.morning.greetings
+			return DateState.morning.dateState
 		}
 	}
 
@@ -274,119 +239,35 @@ class DailyTaskTopView: UIView {
 	}
 }
 
-class FeelingButton: UIControl {
-	var image: UIImage!
-	var selectedImage: UIImage!
-	var title: String!
-	var clickCallback: (() -> Void)!
-	private var isSelectedControl: Bool = false {
-		didSet {
-			if isSelectedControl {
-				circleView.backgroundColor = .mainBlue
-				titleLabel.textColor = .mainBlue
-				imageView.tintColor = .white
-			} else {
-				circleView.backgroundColor = .white
-				titleLabel.textColor = .black
-				imageView.tintColor = .mainBlue
-			}
-		}
-	}
-
-	private var circleView: UIView = {
-		let circleView = UIView()
-		circleView.translatesAutoresizingMaskIntoConstraints = false
-		circleView.backgroundColor = .white
-		circleView.layer.cornerRadius = 28.0
-		circleView.setShadow(shadowRadius: 12.0, shadowColor: .mainLightBlue2!, offset: CGSize(width: 0, height: 18), opacity: 0.5)
-		return circleView
-	}()
-
-	private var imageView: UIImageView = {
-		let imageView = UIImageView()
-		imageView.translatesAutoresizingMaskIntoConstraints = false
-		return imageView
-	}()
-
-	private var titleLabel: UILabel = {
-		let titleLabel = UILabel()
-		titleLabel.translatesAutoresizingMaskIntoConstraints = false
-		return titleLabel
-	}()
-
-	private lazy var actionButton: UIButton = {
-		let actionButton = UIButton()
-		actionButton.translatesAutoresizingMaskIntoConstraints = false
-		actionButton.addTarget(self, action: #selector(onClickFeelingButton), for: .touchUpInside)
-		return actionButton
-	}()
-
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-	}
-
-	convenience init(image: UIImage, selectedImage: UIImage, title: String, callback: @escaping () -> Void) {
-		self.init(frame: .zero)
-		self.image = image
-		self.selectedImage = selectedImage
-		self.title = title
-		self.clickCallback = callback
-		setupViews()
-	}
-
-	@available(*, unavailable)
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-
-	private func setupViews() {
-		addSubview(circleView)
-		circleView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-		circleView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-		circleView.heightAnchor.constraint(equalToConstant: 56.0).isActive = true
-		circleView.widthAnchor.constraint(equalToConstant: 56.0).isActive = true
-
-		circleView.addSubview(imageView)
-		imageView.centerXAnchor.constraint(equalTo: circleView.centerXAnchor).isActive = true
-		imageView.centerYAnchor.constraint(equalTo: circleView.centerYAnchor).isActive = true
-		imageView.heightAnchor.constraint(equalTo: circleView.heightAnchor).isActive = true
-		imageView.widthAnchor.constraint(equalTo: circleView.widthAnchor).isActive = true
-		imageView.tintColor = .blue
-		imageView.image = image
-
-		addSubview(titleLabel)
-		titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-		titleLabel.topAnchor.constraint(equalTo: circleView.bottomAnchor, constant: 16.0).isActive = true
-		titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-		titleLabel.text = title
-
-		addSubview(actionButton)
-		actionButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-		actionButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-		actionButton.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-		actionButton.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
-
-		titleLabel.attributedText = title.attributedString(style: .silkabold14, foregroundColor: .black)
-	}
-
-	@objc func onClickFeelingButton() {
-		isSelectedControl = true
-		imageView.image = selectedImage
-		clickCallback()
-	}
-}
-
 enum DateState {
 	case morning, afternoon, evening
 
-	var greetings: String {
+	var dateState: DateStateModel {
 		switch self {
 		case .morning:
-			return "Good morning"
+			return DateStateModel(
+				iconName: "img-morning",
+				greetings: "Good morning",
+				description: "Remember to take\nsome deep breaths"
+			)
 		case .afternoon:
-			return "Good afternoon"
+			return DateStateModel(
+				iconName: "img-afternoon",
+				greetings: "Good afternoon",
+				description: "Drinking two liters of water a day can\nhelp burn fat and increase energy levels"
+			)
 		case .evening:
-			return "Good evening"
+			return DateStateModel(
+				iconName: "img-evening",
+				greetings: "Good evening",
+				description: "Getting a proper amount of deep sleep\nis essential to your health"
+			)
 		}
 	}
+}
+
+struct DateStateModel {
+	let iconName: String
+	let greetings: String
+	let description: String
 }
